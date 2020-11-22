bda93eb099aedbeb963f925823eb7ed1542a11ce
Kevin Prettre <kevin.prettre@qonto.eu>
2020-11-22 22:41:07 +0100

    Bump version

--- app/app.gradle.kts
+++ app/app.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.wile.main"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
