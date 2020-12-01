1b12e57c4a1a5a95043ee3c5c4e83e593edf21c4
Sylvain Gourdaut <sylvain.gourdaut@adevinta.com>
2020-12-01 10:17:35 +0100

    [DataBinding] Bind the related activity lifecycle for DataBinding LiveData

--- Libraries/Core/src/main/java/com/wile/core/databinding/DataBindingActivity.kt
+++ Libraries/Core/src/main/java/com/wile/core/databinding/DataBindingActivity.kt
@@ -9,5 +9,9 @@ abstract class DataBindingActivity : AppCompatActivity() {
 
     protected inline fun <reified T : ViewDataBinding> binding(
         @LayoutRes resId: Int
-    ): Lazy<T> = lazy { DataBindingUtil.setContentView(this, resId) }
+    ): Lazy<T> = lazy {
+        DataBindingUtil.setContentView<T>(this, resId).apply {
+            setLifecycleOwner { this@DataBindingActivity.lifecycle }
+        }
+    }
 }
